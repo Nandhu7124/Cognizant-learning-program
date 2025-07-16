da6814463b6f75a402ffae000cf64b4346282ae7
Nandhu7124 <125335121+Nandhu7124@users.noreply.github.com>
2025-07-16 23:22:30 +0530

    Add files via upload

---
+++ Week_4_HandsOn/Exercise5/CountryController.java
@@ -0,0 +1,18 @@
+package com.cognizant.springlearn.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.cognizant.springlearn.model.Country;
+import com.cognizant.springlearn.service.CountryService;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        return countryService.getCountry(code);
+    }
+}
