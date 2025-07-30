0d4c585ca6f8e74650ad2137ae5081a55c87e523
Nandhu7124 <125335121+Nandhu7124@users.noreply.github.com>
2025-07-30 21:45:06 +0530

    Add files via upload

---
+++ Week_6_HandsOn/Exercise4/Posts.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Post from './Post';
+
+class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => this.setState({ posts: data }))
+      .catch(error => console.error('Error fetching posts:', error));
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Blog Posts</h2>
+        {this.state.posts.map(post => (
+          <Post key={post.id} title={post.title} body={post.body} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
